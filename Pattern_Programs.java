46a5de41a3783f36143f294fedf3a0f848ca7175
JoshuaBabuRao <joshuababurao@gmail.com>
2021-11-14 21:42:40 +0530

    Pattern Programs

--- Pattern_Programs.java
+++ Pattern_Programs.java
@@ -4,314 +4,367 @@ public class Pattern_Programs
 
 {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		Pattern_Programs p = new Pattern_Programs();
+
 		
-		p.print_pattern_one();
-		System.out.println();
-		
-		p.print_pattern_two();
-		System.out.println();
+		 p.print_pattern_one(); System.out.println();
+		 
+		 
+		  p.print_pattern_two(); System.out.println();
+		  
+		  p.print_pattern_three(); System.out.println();
+		  
+		  p.print_pattern_four(); System.out.println();
+		  
+		  p.print_pattern_five(); System.out.println();
+		  
+		  p.print_pattern_six(); System.out.println();
+		  
+		  p.print_pattern_seven(); System.out.println();
+		  
+		  p.print_pattern_eight(); System.out.println();
+		  
+		  p.print_pattern_nine(); System.out.println();
 
-		p.print_pattern_three();
-		System.out.println();
+		 
 
-		p.print_pattern_four();
-		System.out.println();
-
-		p.print_pattern_five();
-		System.out.println();
-
-		p.print_pattern_six();
-		System.out.println();
 
 	}
-	
-	
-	void print_pattern_one()
+
+	void print_pattern_one() 
 	{
-		for(int j=1;j<=10;j++)
+		for (int j = 1; j <= 10; j++) 
 		{
-		for(int i =1;i<=j;i++)
-		{
-			System.out.print(" "+"*");
-		}
-		System.out.println();
-	
-		}
-		for(int k=9;k>=1;k--)
-		{
-		for(int i =1;i<=k;i++)
-		{
-			System.out.print(" "+"*");
-		}
-		System.out.println();
-		
-	
-	}
-	
-	}
-	
-	
-	
-	void print_pattern_two()
-	{
-		for(int j=10;j>=1;j--)
-		{
-		for(int i =j;i>=1;i--)
-		{
-			System.out.print(" "+" ");
-			
-		}
-	
-		for(int i =10;i>=j;i--)
-		{
-			System.out.print("@"+" ");
-			
-		}
-		for(int i =10;i>=j;i--)
-		{
-			System.out.print("@"+" ");
-			
-		}
-		
-		
-		System.out.println();
-
-		}	
-		
-	}
-	
-
-void print_pattern_three()
-{
-	
-	for(int j=10;j>=1;j--)
-	{
-	for(int i =j;i>=1;i--)
-	{
-		System.out.print(" "+" ");
-		
-	}
-
-	for(int i =10;i>=j;i--)
-	{
-		System.out.print("@"+" ");
-		
-	}
-	for(int i =10;i>=j;i--)
-	{
-		System.out.print("#"+" ");
-		
-	}	
-	System.out.println();
-	}
-	
-	
-	for(int j=1;j<=10;j++)
-		{
-		for(int i =1;i<=j;i++)
-		{
-			System.out.print(" "+" ");
-			
-		}
-		
-		for(int i =10;i>=j;i--)
-		{
-			System.out.print("$"+" ");
-
-		}
-		for(int i=10;i>=j;i--)
-		{
-			System.out.print("&"+" ");
-		}
-		
-		System.out.println();
-		}
-	}
-
-
-
-void print_pattern_four()
-{
-	for(int j=10;j>=1;j--)
-	{
-	for(int i =j;i>=1;i--)
-	{
-		System.out.print(" "+" ");
-		
-	}
-
-	for(int i =10;i>=j;i--)
-	{
-		if(i==10)
-		{
-		System.out.print("@"+" ");
-		}else
-		System.out.print(" "+" ");
-
-	}
-	for(int i =10;i>=j;i--)
-	{
-		if(i==j)
-		{
-		System.out.print("#"+" ");
-		}else
-			System.out.print(" "+" ");
-
-	}	
-	System.out.println();
-	}
-	
-	
-	for(int j=1;j<=10;j++)
-		{
-		for(int i =1;i<=j;i++)
-		{
-			System.out.print(" "+" ");
-			
-		}
-		
-		for(int i =10;i>=j;i--)
-		{
-			if(i==10)
+			for (int i = 1; i <= j; i++)
 			{
-			System.out.print("$"+" ");
-			}
-			else 	
-				System.out.print(" "+" ");
+				System.out.print(" " + "*");
+						}
+			System.out.println();
 
 		}
-		for(int i=10;i>=j;i--)
+		for (int k = 9; k >= 1; k--) 
 		{
-			if(i==j)
+			for (int i = 1; i <= k; i++) 
 			{
-			System.out.print("&"+" ");
+				System.out.print(" " + "*");
+				
 			}
-			else 		
-				System.out.print(" "+" ");
+			System.out.println();
+
+		}
+
+	}
+
+	void print_pattern_two() {
+		for (int j = 10; j >= 1; j--) {
+			for (int i = j; i >= 1; i--) {
+				System.out.print(" " + " ");
 
 			}
-		
-		System.out.println();
+
+			for (int i = 10; i >= j; i--) {
+				System.out.print("@" + " ");
+
+			}
+			for (int i = 10; i >= j; i--) {
+				System.out.print("@" + " ");
+
+			}
+
+			System.out.println();
+
 		}
 
+	}
 
-}
+	void print_pattern_three() {
 
+		for (int j = 10; j >= 1; j--) {
+			for (int i = j; i >= 1; i--) {
+				System.out.print(" " + " ");
 
-void print_pattern_five()
+			}
+
+			for (int i = 10; i >= j; i--) {
+				System.out.print("@" + " ");
+
+			}
+			for (int i = 10; i >= j; i--) {
+				System.out.print("#" + " ");
+
+			}
+			System.out.println();
+		}
+
+		for (int j = 1; j <= 10; j++) {
+			for (int i = 1; i <= j; i++) {
+				System.out.print(" " + " ");
+
+			}
+
+			for (int i = 10; i >= j; i--) {
+				System.out.print("$" + " ");
+
+			}
+			for (int i = 10; i >= j; i--) {
+				System.out.print("&" + " ");
+			}
+
+			System.out.println();
+		}
+	}
+
+	void print_pattern_four() {
+		for (int j = 10; j >= 1; j--) {
+			for (int i = j; i >= 1; i--) {
+				System.out.print(" " + " ");
+
+			}
+
+			for (int i = 10; i >= j; i--) {
+				if (i == 10) {
+					System.out.print("@" + " ");
+				} else
+					System.out.print(" " + " ");
+
+			}
+			for (int i = 10; i >= j; i--) {
+				if (i == j) {
+					System.out.print("#" + " ");
+				} else
+					System.out.print(" " + " ");
+
+			}
+			System.out.println();
+		}
+
+		for (int j = 1; j <= 10; j++) {
+			for (int i = 1; i <= j; i++) {
+				System.out.print(" " + " ");
+
+			}
+
+			for (int i = 10; i >= j; i--) {
+				if (i == 10) {
+					System.out.print("$" + " ");
+				} else
+					System.out.print(" " + " ");
+
+			}
+			for (int i = 10; i >= j; i--) {
+				if (i == j) {
+					System.out.print("&" + " ");
+				} else
+					System.out.print(" " + " ");
+
+			}
+
+			System.out.println();
+		}
+
+	}
+
+	void print_pattern_five() {
+
+		for (int j = 10; j >= 1; j--) {
+			for (int i = j; i >= 1; i--) {
+				System.out.print("@" + " ");
+
+			}
+
+			for (int i = 10; i >= j; i--) {
+				System.out.print(" " + " ");
+
+			}
+			for (int i = 10; i >= j; i--) {
+				System.out.print(" " + " ");
+
+			}
+			for (int i = 1; i <= j; i++) {
+				System.out.print("#" + " ");
+			}
+			System.out.println();
+		}
+
+		for (int j = 1; j <= 10; j++) {
+			for (int i = 1; i <= j; i++) {
+				System.out.print("$" + " ");
+
+			}
+
+			for (int i = 10; i >= j; i--) {
+				System.out.print(" " + " ");
+
+			}
+			for (int i = 10; i >= j; i--) {
+				System.out.print(" " + " ");
+			}
+			for (int i = 1; i <= j; i++) {
+				System.out.print("&" + " ");
+			}
+
+			System.out.println();
+		}
+	}
+
+	void print_pattern_six() {
+		for (int j = 1; j <= 10; j++) {
+			for (int i = 1; i <= j; i++) {
+				System.out.print(" " + "*");
+			}
+			for (int i = 10; i >= j; i--) {
+				System.out.print("@" + " ");
+			}
+			for (int i = j; i <= 10; i++) {
+				System.out.print("~" + " ");
+			}
+			for (int i = 1; i <= j; i++) {
+				System.out.print("&" + " ");
+			}
+			System.out.println();
+
+		}
+
+		for (int k = 10; k >= 1; k--) {
+			for (int i = 1; i <= k; i++) {
+				System.out.print(" " + "^");
+			}
+
+			for (int i = 10; i >= k; i--) {
+
+				System.out.print("#" + " ");
+			}
+			for (int i = k; i <= 10; i++) {
+				System.out.print("+" + " ");
+			}
+			for (int i = 1; i <= k; i++) {
+				System.out.print("?" + " ");
+			}
+
+			System.out.println();
+
+		}
+
+	}
+
+	void print_pattern_seven() {
+
+		for (int j = 0; j <= 9; j++) {
+			for (int i = 0; i <= j; i++) {
+				System.out.print(i + " ");
+			}
+			for (int i = 9; i >= j; i--) {
+				System.out.print(i + " ");
+			}
+			for (int i = j; i <= 9; i++) {
+				System.out.print(i + " ");
+			}
+			for (int i = 0; i <= j; i++) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+
+		}
+
+		for (int k = 9; k >= 0; k--) {
+			for (int i = 0; i <= k; i++) {
+				System.out.print(i + " ");
+			}
+
+			for (int i = 9; i >= k; i--) {
+
+				System.out.print(i + " ");
+			}
+			for (int i = k; i <= 9; i++) {
+				System.out.print(i + " ");
+			}
+			for (int i = 0; i <= k; i++) {
+				System.out.print(i + " ");
+			}
+
+			System.out.println();
+
+		}
+
+	}
+
+	void print_pattern_eight() {
+		for (int j = 20; j >= 10; j--) {
+			for (int i = j; i >= 10; i--) {
+				if (i % 2 == 0) {
+					System.out.print("0" + " ");
+				} else
+					System.out.print("1" + " ");
+			}
+			for (int i = 20; i >= j; i--) {
+
+				if (i % 2 == 0) {
+					System.out.print("0" + " ");
+				} else
+					System.out.print("1" + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+	void print_pattern_nine() 
+	{
+		for (int j = 1; j <= 10; j++) 
+		{
+			for (int i = j; i <= 10; i++)
+			{
+				if (i == 10) 
+				{
+					System.out.print(i);
+				} else
+					System.out.print(" " + " ");
+			}
+			for (int i = 1; i <= j; i++)
+			{
+				System.out.print(" " + " ");
+			}
+			for (int i = 1; i <= j; i++)
+			{
+				System.out.print(" " + " ");
+			}
+			for (int i = 10; i >= j; i--)
+			{
+				if (i == 10)
 {
-	
-	for(int j=10;j>=1;j--)
-	{
-	for(int i =j;i>=1;i--)
-	{
-		System.out.print("@"+" ");
-		
-	}
+					System.out.print(i);
+				} else
+					System.out.print(" " + " ");
+			}
+			for (int i = j; i <= 10; i++)
+			{
+				if (i == 10) 
+				{
+					System.out.print(i);
+					
+				} else
+					System.out.print(" " + " ");
+			}
+			for (int i = 1; i <= j; i++)
+			{
+				System.out.print(" " + " ");
+			}
+			for (int i = 1; i <= j; i++)
+			{
 
-	for(int i =10;i>=j;i--)
-	{
-		System.out.print(" "+" ");
-		
-	}
-	for(int i =10;i>=j;i--)
-	{
-		System.out.print(" "+" ");
-		
-	}	
-	for(int i=1;i<=j;i++)
-	{
-		System.out.print("#"+" ");
-	}
-	System.out.println();
-	}
-	
-	
-	for(int j=1;j<=10;j++)
-		{
-		for(int i =1;i<=j;i++)
-		{
-			System.out.print("$"+" ");
-			
-		}
-		
-		for(int i =10;i>=j;i--)
-		{
-			System.out.print(" "+" ");
-
-		}
-		for(int i=10;i>=j;i--)
-		{
-			System.out.print(" "+" ");
-		}
-		for(int i=1;i<=j;i++)
-		{
-			System.out.print("&"+" ");
-		}
-			
-		System.out.println();
-		}
-	}
-
-
-void print_pattern_six()
+				System.out.print(" " + " ");
+			}
+			for (int i = 10; i >= j; i--)
+			{
+				if (i == 10)
 {
-	for(int j=1;j<=10;j++)
-	{
-	for(int i =1;i<=j;i++)
-	{
-		System.out.print(" "+"*");
-	}
-	for(int i =j;i<=10;i++)
-	{
-		System.out.print("@"+" ");
-	}
-	for(int i =j;i<=10;i++)
-	{
-		System.out.print("~"+" ");
-	}
-	for(int i =1;i<=j;i++)
-	{
-		System.out.print("&"+ " ");
-	}
-	System.out.println();
+					System.out.print(i);
+				} else
+					System.out.print(" " + " ");
+			}
+			System.out.println();
+		}
 
 	}
-	
-	for(int k=10;k>=1;k--)
-	{
-	for(int i =1;i<=k;i++)
-	{
-		System.out.print(" "+"^");
-	}
-	
-	  for(int i =10;i>=k;i--) 
-	  {
-	  
-	  System.out.print("#"+" "); 
-	  }
-	  for(int i =k;i<=10;i++)
-	  {
-		System.out.print("+"+" ");  
-	  }
-	  for(int i =1;i<=k;i++)
-	  {
-		  System.out.print("?"+" ");
-	  }
-	  
-	System.out.println();
-	
-
 }
-
-
-
-}
-
-
-
-}
-
